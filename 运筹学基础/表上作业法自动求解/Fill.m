747c16863f2bfb4ead3d05934610235632a4cf95
徐亦昶 <71507738+Kobe972@users.noreply.github.com>
2022-04-12 00:21:53 +0800

    Add files via upload

---
+++ 运筹学基础/表上作业法自动求解/Fill.m
@@ -0,0 +1,36 @@
+function f=Fill(cost,a,b)
+chart=inf*ones(length(a(:)),length(b(:)));
+for cnt=1:length(a(:))+length(b(:))-1
+    minimum=min(min(cost));
+    if(minimum==inf)
+        continue;
+    end
+    [u,v]=find(cost==minimum);
+    u=u(1);
+    v=v(1);
+    chart(u,v)=min(a(u),b(v));
+    if(a(u)>b(v)) %mask the column
+        for row=1:length(a(:))
+            cost(row,v)=inf;
+        end
+        a(u)=a(u)-b(v);
+    else %mask the row
+        for col=1:length(b(:))
+            cost(u,col)=inf;
+        end
+        b(v)=b(v)-a(u);
+    end
+end
+f=chart;
+fprintf("初始基可行解为：\n");
+for i=1:length(chart(:,1))
+    for j=1:length(chart)
+        if(chart(i,j)~=inf)
+            fprintf("%d\t",chart(i,j));
+        else
+            fprintf("-\t");
+        end
+    end
+    fprintf("\b\n");
+end
+return;
---
+++ 运筹学基础/表上作业法自动求解/solve.m
@@ -0,0 +1,22 @@
+function f=solve(cost,a,b,chart)
+if nargin==3
+    chart=Fill(cost,a,b);
+end
+exam=GetC(cost,chart);
+while(min(min(exam))<0)
+    chart=Adjust(chart,exam);
+    exam=GetC(cost,chart);
+end
+fprintf("检验数均不小于0，达到最优。\n");
+sum=0;
+fprintf("故最小值为");
+for i=1:length(chart(:,1))
+    for j=1:length(chart)
+        if(chart(i,j)~=inf)
+            fprintf("%d×%d+",chart(i,j),cost(i,j));
+            sum=sum+chart(i,j)*cost(i,j);
+        end
+    end
+end
+fprintf("\b=%d。\n",sum);
+return;
